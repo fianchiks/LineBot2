ad9b79c3f4c0e4529cbffc066c1281bfa685f9e0
lalauya1122 <lalauya1122@gmail.com>
2017-10-24 21:44:52 +0800

    git clone https://github.com/lalauya1122/LineBot2

--- line-tcr.py
+++ line-tcr.py
@@ -81,9 +81,9 @@ Prefix is Case sensitive but the commands is not.
 """
 
 KAC=[cl,ki,kk,kc]
-mid = cl.getProfile().mid
-Amid = kk.getProfile().mid
-Bmid = ki.getProfile().mid
+mid = cl.getProfile(u6de2fd0d3f168038814531c1fb8fb7dc).
+Amid = kk.getProfile(u121d2c4491af367a654beff87dce04ca).
+Bmid = ki。getProfile（）。中
 Cmid = kc.getProfile().mid
 Bots = [mid,Amid,Bmid,Cmid,]
 admin = ["u6de2fd0d3f168038814531c1fb8fb7dc"]
